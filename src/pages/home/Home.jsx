7df03e13588d1d4916a8391170eabfbf24048cf3
Lino Khan <Lino Khan>
2024-10-19 16:19:44 +0200

    feat(home-page): added hero text

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,11 +4,122 @@ const Home = () => {
 
     return (
         <>
-        <div className="home-page">
-            <div className="main">
-                {/** Hero Section */}
-                <section className="section">
-                    <div className="hero-section">
+            <div className="home-page">
+                <div className="main">
+                    {/** Hero Section */}
+                    <section className="section">
+                        <div className="hero-section">
+                            <div className="row">
+                                <div className="col">
+                                    <h1>
+                                        Healthcare just got less painful
+                                    </h1>
+                                    <p>
+                                        Now Amazon Prime members can add a One Medical membership for only $9/month or $99/year.*
+                                    </p>
+                                    <button>Get started on Amazon</button>
+                                    <p>
+                                        *Purchase terms apply.
+                                    </p>
+                                    <p>
+                                        Already a One Medical member? Follow these steps to apply the Amazon Prime savings.
+                                    </p>
+                                    <p>
+                                        Have a company sponsored membership? Click here.
+                                    </p>
+
+                                </div>
+                                <div className="col">
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </section>
+
+                    {/** Services Section */}
+                    <section className="section">
+                        <div className="services-section">
+                            <div className="row">
+                                <div className="col">
+
+                                </div>
+                                <div className="col">
+
+                                </div>
+                                <div className="col">
+
+                                </div>
+                            </div>
+                            <div className="row">
+
+                            </div>
+                        </div>
+
+                    </section>
+
+                    {/** About Section */}
+                    <section className="section">
+                        <div className="about-section">
+                            <div className="row">
+                                <div className="col">
+
+                                </div>
+                                <div className="col">
+
+
+                                </div>
+                                <div className="col">
+
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+
+                                </div>
+                                <div className="col">
+
+
+                                </div>
+                                <div className="col">
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </section>
+
+                    {/** Membership Section */}
+                    <section className="section">
+                        <div className="membership-section">
+                            <div className="row">
+                                <div className="col">
+
+                                </div>
+                                <div className="col">
+
+                                </div>
+                            </div>
+                            <div className="row">
+
+                            </div>
+                        </div>
+
+                    </section>
+
+                    {/** Testimonial Section */}
+                    <section className="section">
+                        <div className="testimonial-section">
+
+                        </div>
+
+                    </section>
+
+                    {/** Partners Section */}
+                    <section className="section">
                         <div className="row">
                             <div className="col">
 
@@ -16,20 +127,6 @@ const Home = () => {
                             <div className="col">
 
                             </div>
-
-                        </div>
-
-                    </div>
-
-                </section>
-
-                {/** Services Section */}
-                <section className="section">
-                    <div className="services-section">
-                        <div className="row">
-                            <div className="col">
-
-                            </div>
                             <div className="col">
 
                             </div>
@@ -37,97 +134,16 @@ const Home = () => {
 
                             </div>
                         </div>
-                        <div className="row">
-
-                        </div>
-                    </div>
-
-                </section>
-
-                {/** About Section */}
-                <section className="section">
-                    <div className="about-section">
-                        <div className="row">
-                            <div className="col">
-
-                            </div>
-                            <div className="col">
-
-
-                            </div>
-                            <div className="col">
-
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col">
-
-                            </div>
-                            <div className="col">
-
-
-                            </div>
-                            <div className="col">
-                                
-                            </div>
-                        </div>
-
-                    </div>
-
-                </section>
-
-                {/** Membership Section */}
-                <section className="section">
-                    <div className="membership-section">
-                        <div className="row">
-                            <div className="col">
-
-                            </div>
-                            <div className="col">
-
-                            </div>
-                        </div>
-                        <div className="row">
-
-                        </div>
-                    </div>
-
-                </section>
-
-                {/** Testimonial Section */}
-                <section className="section">
-                    <div className="testimonial-section">
-
-                    </div>
-
-                </section>
-
-                {/** Partners Section */}
-                <section className="section">
-                    <div className="row">
-                        <div className="col">
-
-                        </div>
-                        <div className="col">
-
-                        </div>
-                        <div className="col">
-
-                        </div>
-                        <div className="col">
-                            
-                        </div>
-                    </div>
-                </section>
+                    </section>
 
 
 
+
+                </div>
 
             </div>
-
-        </div>
         </>
-        
+
     );
 
 }
