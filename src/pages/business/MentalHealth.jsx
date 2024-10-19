62107bd3e24fcdcf8be374ac618f83984fd0f9b6
Lino Khan <Lino Khan>
2024-10-19 15:42:02 +0200

    routing added & dependencies installed

---
+++ src/pages/business/MentalHealth.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MentalHealth = () => {
+
+    return (
+        <div>
+            <h1>Mental Health Page</h1>
+        </div>
+    );
+
+}
+
+export default MentalHealth;
---
+++ src/pages/business/Results.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Result = () => {
+
+    return (
+        <div>
+            <h1>Results Page</h1>
+        </div>
+    );
+
+}
+
+export default Result;
