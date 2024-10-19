9cc6993a8617eb97d52397dc6e4c294ce97023a9
Lino Khan <Lino Khan>
2024-10-19 16:13:00 +0200

    feat(home-page): added page structure

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,9 +3,131 @@ import React from "react";
 const Home = () => {
 
     return (
-        <div>
-            <h1>Home Page</h1>
+        <>
+        <div className="home-page">
+            <div className="main">
+                {/** Hero Section */}
+                <section className="section">
+                    <div className="hero-section">
+                        <div className="row">
+                            <div className="col">
+
+                            </div>
+                            <div className="col">
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </section>
+
+                {/** Services Section */}
+                <section className="section">
+                    <div className="services-section">
+                        <div className="row">
+                            <div className="col">
+
+                            </div>
+                            <div className="col">
+
+                            </div>
+                            <div className="col">
+
+                            </div>
+                        </div>
+                        <div className="row">
+
+                        </div>
+                    </div>
+
+                </section>
+
+                {/** About Section */}
+                <section className="section">
+                    <div className="about-section">
+                        <div className="row">
+                            <div className="col">
+
+                            </div>
+                            <div className="col">
+
+
+                            </div>
+                            <div className="col">
+
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+
+                            </div>
+                            <div className="col">
+
+
+                            </div>
+                            <div className="col">
+                                
+                            </div>
+                        </div>
+
+                    </div>
+
+                </section>
+
+                {/** Membership Section */}
+                <section className="section">
+                    <div className="membership-section">
+                        <div className="row">
+                            <div className="col">
+
+                            </div>
+                            <div className="col">
+
+                            </div>
+                        </div>
+                        <div className="row">
+
+                        </div>
+                    </div>
+
+                </section>
+
+                {/** Testimonial Section */}
+                <section className="section">
+                    <div className="testimonial-section">
+
+                    </div>
+
+                </section>
+
+                {/** Partners Section */}
+                <section className="section">
+                    <div className="row">
+                        <div className="col">
+
+                        </div>
+                        <div className="col">
+
+                        </div>
+                        <div className="col">
+
+                        </div>
+                        <div className="col">
+                            
+                        </div>
+                    </div>
+                </section>
+
+
+
+
+            </div>
+
         </div>
+        </>
+        
     );
 
 }
