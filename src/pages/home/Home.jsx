657d5eeb89565e049901ece9f30b9d338d366edb
Lino Khan <Lino Khan>
2024-10-22 20:13:19 +0200

    feat(home-page): completed structure, adding text, images & initial CSS

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './home.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Care_one from '../../assets/images/care_one.png';
 import Care_two from '../../assets/images/care_two.png';
 import Care_three from '../../assets/images/care_three.png';
+import Membership from '../../assets/images/membership.png';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { faMedrt, faAirbnb, faGoogle, faMedium } from '@fortawesome/free-brands-svg-icons';
+
+
+
 
 const Home = () => {
 
@@ -12,8 +19,9 @@ const Home = () => {
             <div className="home-page">
                 <div className="main">
                     {/** Hero Section */}
-                    <section className="section">
-                        <div className="hero-section">
+                    <div className="hero-section">
+                        <section className="section">
+
                             <div className="row">
                                 <div className="col">
                                     <h1 className="display-1">
@@ -40,17 +48,18 @@ const Home = () => {
                                 </div>
 
                             </div>
+                        </section>
 
-                        </div>
 
-                    </section>
+                    </div>
+
 
                     {/** Services Section */}
 
                     <div className="services-section">
                         <section className="section">
                             <div className="row service-preview">
-                              
+
                                 <div className="col">
                                     <img className="img-fluid" src={Care_three} alt="" />
                                     <h1>
@@ -83,7 +92,11 @@ const Home = () => {
                             <div className="row view-services">
                                 <h1 className="display-1">
 
-                                    Get care today for vaccinations
+                                    Get care today for
+                                </h1>
+                                <h1 className="display-1">
+                                    erectile dysfunction
+
                                 </h1>
                                 <h3>
                                     See all our services
@@ -98,31 +111,35 @@ const Home = () => {
                     {/** About Section */}
                     <section className="section">
                         <div className="about-section">
-                            <h1 className="display-1">
-                                Fall in love with your doctor’s office
-                            </h1>
-                            <p>
-                                We accept most major health insurance plans for scheduled in-office and remote visits just like a typical doctor’s office, but we make it faster, easier, and more enjoyable to get care.
+                            <div className="preview">
+                                <h1 className="display-1">
+                                    Fall in love with your doctor’s office
+                                </h1>
+                                <p>
+                                    We accept most major health insurance plans for scheduled in-office and remote visits just like a typical doctor’s office, but we make it faster, easier, and more enjoyable to get care.
 
 
-                            </p>
-                            <h3> See if we're in network with your insurance</h3>
+                                </p>
+                                <h3> See if we're in network with your insurance</h3>
+
+                            </div>
+
                             <div className="row">
                                 <div className="col">
-                                    <img src="" alt="" />
+                                    <FontAwesomeIcon icon={faMedrt} style={{ color: '#00ab6c', fontSize: '5rem' }} />
                                     <p>Same/next-day appointments, in person or over video, that start on time</p>
                                     <h3>Learn more</h3>
 
                                 </div>
                                 <div className="col">
-                                    <img src="" alt="" />
+                                    <FontAwesomeIcon icon={faMedrt} style={{ color: '#00ab6c', fontSize: '5rem' }} />
                                     <p>Longer appointments so you don’t feel rushed</p>
                                     <h3>Discover the difference</h3>
 
 
                                 </div>
                                 <div className="col">
-                                    <img src="" alt="" />
+                                    <FontAwesomeIcon icon={faMedrt} style={{ color: '#00ab6c', fontSize: '5rem' }} />
                                     <p>Drop-in lab services at our offices to save you extra trips</p>
                                     <h3>Explore lab services</h3>
 
@@ -130,20 +147,20 @@ const Home = () => {
                             </div>
                             <div className="row">
                                 <div className="col">
-                                    <img src="" alt="" />
+                                    <FontAwesomeIcon icon={faMedrt} style={{ color: '#00ab6c', fontSize: '5rem' }} />
                                     <p>Covering everything from physicals and chronic care to mental health</p>
                                     <h3>Explore care</h3>
 
                                 </div>
                                 <div className="col">
-                                    <img src="" alt="" />
+                                    <FontAwesomeIcon icon={faMedrt} style={{ color: '#00ab6c', fontSize: '5rem' }} />
                                     <p>Primary care offices in many US cities</p>
                                     <h3>Visit us</h3>
 
 
                                 </div>
                                 <div className="col">
-                                    <img src="" alt="" />
+                                    <FontAwesomeIcon icon={faMedrt} style={{ color: '#00ab6c', fontSize: '5rem' }} />
                                     <p>
 
                                         Compassionate providers who are experts at medicine and caring
@@ -158,47 +175,76 @@ const Home = () => {
                     </section>
 
                     {/** Membership Section */}
-                    <section className="section">
-                        <div className="membership-section">
-                            <div className="row">
+                    <div className="membership-section">
+                        <section className="section">
+
+                            <div className="row list-options">
                                 <div className="col">
                                     <h1>
                                         Membership gives you access to our app where you can:
                                     </h1>
                                     <ul>
                                         <li>
-                                            Get 24/7 on-demand care nationwide via Video Chat or Treat Me Now at no extra cost
+                                            <span><FontAwesomeIcon className="check" icon={faCheckCircle} />
+                                                Get 24/7 on-demand care nationwide via Video Chat or Treat Me Now at no extra cost
+                                            </span>
+
                                         </li>
-                                        <li>Send and receive secure messages with a provider</li>
-                                        <li>Easily access your health records and care plans</li>
-                                        <li>Request prescription refills and renewals</li>
-                                        <li>Book an in-person or remote visit in a state where we have offices</li>
+                                        <li>
+                                            <span><FontAwesomeIcon className="check" icon={faCheckCircle} />
+                                                Get 24/7 on-demand care nationwide via Video Chat or Treat Me Now at no extra cost
+                                            </span>
+
+                                        </li>
+                                        <li>
+                                            <span><FontAwesomeIcon className="check" icon={faCheckCircle} />
+                                                Get 24/7 on-demand care nationwide via Video Chat or Treat Me Now at no extra cost
+                                            </span>
+
+                                        </li>
+                                        <li>
+                                            <span><FontAwesomeIcon className="check" icon={faCheckCircle} />
+                                                Get 24/7 on-demand care nationwide via Video Chat or Treat Me Now at no extra cost
+                                            </span>
+
+                                        </li>
+                                        <li>
+                                            <span><FontAwesomeIcon className="check" icon={faCheckCircle} />
+                                                Get 24/7 on-demand care nationwide via Video Chat or Treat Me Now at no extra cost
+                                            </span>
+
+                                        </li>
                                     </ul>
+                                    <h3>Learn more about One Medical membership</h3>
 
                                 </div>
                                 <div className="col">
-                                    <img src="" alt="" />
+                                    <img className="img-fluid" src={Membership} alt="" />
 
                                 </div>
                             </div>
-                            <div className="row">
-                                <h1>Membership costs only $199/year.</h1>
+                            <div className="row join">
+                                <h1 className="display-1">Membership costs only $199/year.</h1>
                                 <h2>Learn more about membership </h2>
                                 <button>Join now</button>
 
                             </div>
-                        </div>
+                        </section>
 
-                    </section>
+                    </div>
+
 
                     {/** Testimonial Section */}
-                    <section className="section">
-                        <div className="testimonial-section">
+
+                    <div className="testimonial-section">
+                        <section className="section">
                             <h1>See what makes One Medical different</h1>
+                            <div className="testimonial-video">
 
-                        </div>
+                            </div>
+                        </section>
+                    </div>
 
-                    </section>
 
                     {/** Partners Section */}
                     <section className="section">
@@ -207,18 +253,22 @@ const Home = () => {
                             </h1>
                             <div className="row">
 
-                                <div className="col">
-
+                                <div className="brand-icons">
+                                    <span className="icon">
+                                        <FontAwesomeIcon icon={faAirbnb} size="4x" />
+                                    </span>
+                                    <span className="icon">
+                                        <FontAwesomeIcon icon={faGoogle} size="4x" />
+                                    </span>
+                                    <span className="icon">
+                                        <FontAwesomeIcon icon={faMedrt} size="4x" />
+                                    </span>
+                                    <span className="icon">
+                                        <FontAwesomeIcon icon={faMedium} size="4x" />
+                                    </span>
                                 </div>
-                                <div className="col">
+                                <h3>Learn more about One Medical membership</h3>
 
-                                </div>
-                                <div className="col">
-
-                                </div>
-                                <div className="col">
-
-                                </div>
                             </div>
 
                         </div>
