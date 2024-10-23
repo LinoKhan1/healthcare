058781b3314c32c5172fc50c2ee550dc1dd22732
Lino Khan <Lino Khan>
2024-10-23 10:27:51 +0200

    feat(footer-component): added basic structure on footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,12 +1,48 @@
 import React from "react";
+import './layout.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Footer =()=>{
-    return(
+
+
+const Footer = () => {
+    return (
 
         <>
-        <div className="footer-component">
-            <h1>Footer</h1>
-        </div>
+            <div className="footer-component">
+                <section className="section">
+                    <div className="membership-footer-section">
+                        <h1 className="display-1">
+                            Sign up for membership
+                        </h1>
+                        <button>
+                            Join now
+                        </button>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <ul>
+                                <li>Blog</li>
+                                <li>Media</li>
+                                <li>Contact</li>
+                                <li>Overview</li>
+                                <li>Mental Health</li>
+
+                            </ul>
+
+                        </div>
+                        <div className="col">
+                            <div className="social-icons">
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                </section>
+
+            </div>
         </>
 
     );
