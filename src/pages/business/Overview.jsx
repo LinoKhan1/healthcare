8d7860b2709b3b69bf6573955e06aad552a682e5
Lino Khan <Lino Khan>
2024-10-24 13:04:35 +0200

    feat(business-overview-page): added page structure & bootstrap classes

--- src/pages/business/Overview.jsx
+++ src/pages/business/Overview.jsx
@@ -3,9 +3,182 @@ import React from "react";
 const Overview = () => {
 
     return (
-        <div>
-            <h1>Overview Page</h1>
-        </div>
+        <>
+            <div className="overview-page">
+                <div className="main">
+
+                    {/** Hero Section */}
+                    <div className="hero-section">
+                        <section className="section">
+                            <div className="row">
+                                <div className="col-lg-6">
+
+                                </div>
+                                <div className="col-lg-6">
+
+                                </div>
+                            </div>
+
+
+                        </section>
+                    </div>
+                    {/** About Section */}
+                    <div className="about-section">
+                        <section className="section">
+                            <div className="title">
+
+                            </div>
+                            <div className="row">
+                                <div className="col">
+
+                                </div>
+                                <div className="col">
+
+                                </div>
+                                <div className="col">
+
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+
+                                </div>
+                                <div className="col">
+
+                                </div>
+                            </div>
+
+                        </section>
+                    </div>
+                    {/** Offer Section */}
+                    <div className="offer-section">
+                        <section className="section">
+                            <div className="row">
+                                <div className="col-lg-9">
+
+                                </div>
+                                <div className="col-lg-3">
+
+                                </div>
+                            </div>
+
+
+                        </section>
+                    </div>
+                    {/** Industry Section */}
+                    <div className="industry-section">
+                        <section className="section">
+                            <div className="title">
+
+                            </div>
+                            <div className="carousel">
+
+                            </div>
+
+
+                        </section>
+                    </div>
+                    {/** Value Section */}
+                    <div className="value-section">
+                        <section className="section">
+                            <div className="title">
+
+                            </div>
+                            <div className="row">
+                                <div className="col">
+
+                                </div>
+                                <div className="col">
+
+                                </div>
+                                <div className="col">
+
+                                </div>
+                            </div>
+                            <div className="row view-results">
+                                <div className="col-lg-9">
+
+                                </div>
+                                <div className="col-lg-3">
+
+                                </div>
+
+                            </div>
+
+                        </section>
+                    </div>
+                    {/** Testimonial Section */}
+                    <div className="testimonial-section">
+                        <section className="section">
+                            <div className="carousel">
+
+                            </div>
+
+                        </section>
+                    </div>
+                    {/** Access Section */}
+                    <div className="access-section">
+                        <section className="section">
+                            <div className="row">
+                                <div className="col-lg-6">
+
+                                </div>
+                                <div className="col-lg-6">
+
+                                </div>
+                            </div>
+
+                        </section>
+                    </div>
+                    {/** Blog Section */}
+                    <div className="blog-section">
+                        <section className="section">
+                            <div className="blog-preview">
+                                <div className="title">
+
+                                </div>
+                                <div className="row">
+                                    <div className="col">
+
+                                    </div>
+                                    <div className="col">
+
+                                    </div>
+                                    <div className="col">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="blogs">
+                                <div className="title">
+
+                                </div>
+                                <div className="row">
+                                    <div className="col">
+
+                                    </div>
+                                    <div className="col">
+
+                                    </div>
+                                    <div className="col">
+
+                                    </div>
+                                </div>
+                            </div>
+
+                        </section>
+                    </div>
+                    {/** Contact Section */}
+                    <div className="contact-section">
+                        <section className="section">
+
+                        </section>
+                    </div>
+                </div>
+
+            </div>
+
+        </>
     );
 
 }
