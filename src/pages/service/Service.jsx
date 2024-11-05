d5a9af3d646361af43353b6578f7c30ef727a236
Lino Khan <Lino Khan>
2024-11-05 16:27:30 +0200

    feat(services-page): added page structure

---
+++ src/pages/service/Service.jsx
@@ -0,0 +1,87 @@
+// React
+import React from 'react';
+
+
+const Service = () => {
+
+
+    return (
+        <div className="service-page">
+            <div className="main">
+                {/** Hero Section */}
+                <div className="hero-section">
+                    <section className="section">
+
+                    </section>
+
+                </div>
+                {/** Service Section */}
+                <div className="service-section">
+
+                    <div className="service-preview">
+                        <div className="title">
+                            <h1>
+
+                            </h1>
+                            <p>
+
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="service-list">
+                    <div className="row">
+                        <div className="col">
+
+                        </div>
+                        <div className="col">
+
+                        </div>
+
+                    </div>
+                    <div className="row">
+                        <div className="col">
+
+                        </div>
+                        <div className="col">
+
+                        </div>
+
+                    </div>
+                    <div className="row">
+                        <div className="col">
+
+                        </div>
+                        <div className="col">
+
+                        </div>
+
+                    </div>
+                    <div className="row">
+                        <div className="col">
+
+                        </div>
+                        <div className="col">
+
+                        </div>
+
+                    </div>
+                    <div className="row">
+                        <div className="col">
+
+                        </div>
+                        <div className="col">
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+
+    );
+}
