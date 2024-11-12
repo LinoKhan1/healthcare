659cd3f9cbe6462a5902b8939d0b5e676df27181
Lino Khan <Lino Khan>
2024-11-12 17:46:18 +0200

    main: added netlify.toml

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    assetsInlineLimit: 0, // Set to 0 to disable inlining and ensure all assets are copied
+  },
+});
